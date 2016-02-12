764c97b0fa1bb933e2ab7306956e176afb2fa574
Prabhu vignesh Rajagopal <prabhuvigneshrajagopal@Prabhus-Mac-mini.local>
2016-02-12 23:07:42 +0530

    update

--- company.rb
+++ company.rb
@@ -14,22 +14,41 @@ class Company
 	def quantity
 		@quantity
 	end
+	def after_buy
+		@after_buy
+	end
+
+	def after_sell
+		@after_sell
+	end
 
 	def buy(qty)
-		@after_buy = self.quantity + qty
-		self.update(self.company_name,@after_buy)
+		if self.after_sell == 0
+			@status_buy = 'close'
+			
+		else
+			@after_buy = self.after_sell - qty
+			@status_buy = 'open'
+		end
 	end
 
 	def sell(qty)
-		@after_sell =  self.quantity - qty
-		self.update(self.company_name,@after_sell)
-	end
-	def status
-		if @quantity > 0			
-			@status = 'open'
+		if self.after_buy == 0
+			@status_sell = 'close'
 		else
-			@status = 'close'
+			@after_sell = self.after_buy - qty
+			@status_sell = 'open'
 		end
+		@after_sell = self.after_sell != nil ? self.after_sell - self.after_buy : qty
+		self.update(self.company_name,@after_buy)
+	end
+	
+	def status_buy
+		@status_buy
+	end
+
+	def status_sell			
+		@status_sell
 	end
 end
 
@@ -38,36 +57,36 @@ XYZ = Company.new('XYZ',0)
 
 
 # ============ company ABC 1st iter ================
-p "============ company #{ABC.company_name}  1st iter ================"
+p "============ company #{ABC.company_name} buy 10  1st iter ================"
 
 ABC.buy(10)
-p ABC.quantity
-p ABC.status
+p ABC.after_buy
+p ABC.status_buy
 
 # ============ company XYZ 2st iter ================
-p "============ company #{ XYZ.company_name }2st iter ================"
+p "============ company #{ XYZ.company_name } sell 15 2st iter ================"
 
-ABC.sell(15)
-p ABC.quantity
-p ABC.status
+XYZ.sell(15)
+p XYZ.after_sell
+p XYZ.status_sell
 
 # ============ company ABC 3st iter ================
-p "============ company #{ABC.company_name}  2st iter ================"
+p "============ company #{ABC.company_name} sell 13 3rd iter ================"
 
 ABC.sell(13)
-p ABC.quantity
-p ABC.status
+p ABC.after_sell
+p ABC.status_sell
 
 # ============ company XYZ 4st iter ================
-p "============ company #{ XYZ.company_name} 2st iter ================"
+p "============ company #{ XYZ.company_name} buy 10  2st iter ================"
 
-ABC.buy(10)
-p ABC.quantity
-p ABC.status
+XYZ.buy(10)
+p XYZ.after_buy
+p XYZ.status_buy
 
 # ============ company XYZ 5st iter ================
-p "============ company #{XYZ.company_name}  2st iter ================"
+p "============ company #{XYZ.company_name} buy 8 2st iter ================"
 
-ABC.buy(8)
-p ABC.quantity
-p ABC.status
+XYZ.buy(8)
+p XYZ.after_buy
+p XYZ.status_buy
